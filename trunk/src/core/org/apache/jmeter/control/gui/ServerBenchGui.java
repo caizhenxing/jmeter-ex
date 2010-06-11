05b3ad638c40fdf70770e668ac80d6f5692a5306
ycc9958 <ycc9958@c5ef9ad6-2f23-11df-9f25-831aa717a150>
2010-06-11 16:18:03 +0000

    add aliper login component

--- trunk/src/core/org/apache/jmeter/control/gui/ServerBenchGui.java
+++ trunk/src/core/org/apache/jmeter/control/gui/ServerBenchGui.java
@@ -1,18 +1,22 @@
 package org.apache.jmeter.control.gui;
 
-import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.util.Collection;
 
-import javax.swing.JMenu;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPopupMenu;
+import javax.swing.JTextField;
 
 import org.apache.jmeter.gui.AbstractJMeterGuiComponent;
-import org.apache.jmeter.gui.action.ActionNames;
-import org.apache.jmeter.gui.util.MenuFactory;
-import org.apache.jmeter.server.gui.ServerGui;
+import org.apache.jmeter.gui.util.VerticalPanel;
 import org.apache.jmeter.testelement.ServerBench;
 import org.apache.jmeter.testelement.TestElement;
-import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.gui.layout.VerticalLayout;
 
 /**
  * Server Bench
@@ -23,6 +27,10 @@ import org.apache.jmeter.util.JMeterUtils;
 public class ServerBenchGui extends AbstractJMeterGuiComponent {
 	private static final long serialVersionUID = 1L;
 
+	private JComboBox com = new JComboBox();
+	private JTextField rangeField = new JTextField(58);
+	private JButton update = new JButton("update");
+	private JButton connect = new JButton("connect");
 	/**
 	 * Create a new JVMbenchGui.
 	 */
@@ -59,11 +67,11 @@ public class ServerBenchGui extends AbstractJMeterGuiComponent {
 	 */
 	public JPopupMenu createPopupMenu() {
 		JPopupMenu menu = new JPopupMenu();
-		JMenu addMenu = new JMenu(JMeterUtils.getResString("add"));
-		addMenu.add(MenuFactory.makeMenuItem(JMeterUtils
-				.getResString("monitor_server"), ServerGui.class.getName(),
-				ActionNames.ADD));
-		menu.add(addMenu);
+//		JMenu addMenu = new JMenu(JMeterUtils.getResString("add"));
+//		addMenu.add(MenuFactory.makeMenuItem(JMeterUtils
+//				.getResString("monitor_server"), ServerGui.class.getName(),
+//				ActionNames.ADD));
+//		menu.add(addMenu);
 		return menu;
 	}
 
@@ -75,9 +83,44 @@ public class ServerBenchGui extends AbstractJMeterGuiComponent {
 	 * Initialize the components and layout of this component.
 	 */
 	private void init() {
-		setLayout(new BorderLayout());
+		setLayout(new VerticalLayout(5, VerticalLayout.BOTH));
 		setBorder(makeBorder());
 
-		add(makeTitlePanel(), BorderLayout.NORTH);
+		add(makeTitlePanel());
+
+		// URL
+		VerticalPanel mainPanel = new VerticalPanel();
+
+		mainPanel.setLayout(new VerticalLayout(5, VerticalLayout.LEFT));
+		mainPanel.setBorder(BorderFactory.createTitledBorder("登陆服务器"));
+
+		// project
+		Box urlsPanel = Box.createHorizontalBox();
+		JLabel urls=new JLabel("Server URL:");
+		urls.setPreferredSize(new Dimension(80,20));
+		urlsPanel.add(urls);
+
+		urlsPanel.add(rangeField);
+
+		mainPanel.add(urlsPanel);
+
+		Box projectPanel = Box.createHorizontalBox();
+		JLabel pro=new JLabel("Projects:");
+		pro.setPreferredSize(new Dimension(80,20));
+		projectPanel.add(pro);
+
+		String s[]={"press the update button to get the info of projects'"};
+		com.setModel(new DefaultComboBoxModel(s));
+		com.setPreferredSize(new Dimension(300, 20));
+		projectPanel.add(com);
+		projectPanel.add(Box.createHorizontalStrut(5));
+		projectPanel.add(update);
+		mainPanel.add(projectPanel);
+		
+		Box buttonPanel = Box.createHorizontalBox();
+		buttonPanel.add(Box.createHorizontalStrut(20));
+		buttonPanel.add(connect);
+		mainPanel.add(buttonPanel);
+		add(mainPanel);
 	}
 }
